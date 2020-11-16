7f5a70a75b77eb2f9eb8736da58915ebcfa85246
RodrigoR17 <2180639@my.ipleiria.pt>
2020-11-16 21:59:34 +0000

    OFICINA-23 marcacao

--- OficinaESTG/frontend/views/marcacao/_form.php
+++ OficinaESTG/frontend/views/marcacao/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\jui\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Marcacao */
@@ -16,13 +17,17 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'tipoMarcacao')->dropDownList([ 'Reparacao' => 'Reparacao', 'Venda' => 'Venda', ], ['prompt' => '']) ?>
 
-    <?= $form->field($model, 'dataMarcacao')->textInput() ?>
+    <?= $form->field($model, 'dataMarcacao')->widget(\yii\jui\DatePicker::classname(), [
+        'options' => ['class' => 'form-control'],
+        'language' => 'pt',
+        'dateFormat' => 'yyyy-MM-dd',
+    ]) ?>
 
     <?= $form->field($model, 'descricaoMarcacao')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'estadoMarcacao')->dropDownList([ 'Aceite' => 'Aceite', 'Rejeitada' => 'Rejeitada', 'Concluida' => 'Concluida', 'Espera' => 'Espera', ], ['prompt' => '']) ?>
 
-    <?= $form->field($model, 'fk_idPessoa')->textInput() ?>
+    <?= $form->field($model, 'fk_idPessoa')->hiddenInput(['value'=>Yii::$app->user->id])->label(false) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
