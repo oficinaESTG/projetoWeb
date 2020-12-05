566952dd9b383d10194452cf0c609822fb212603
Rodrigo <48674118+RodrigoR17@users.noreply.github.com>
2020-12-05 16:47:15 +0000

    OFICINA-17 teste de login fucntional feito

--- OficinaESTG/frontend/tests/functional/LoginCest.php
+++ OficinaESTG/frontend/tests/functional/LoginCest.php
@@ -1,14 +1,45 @@
 <?php namespace frontend\tests\functional;
+use common\fixtures\UserFixture;
 use frontend\tests\FunctionalTester;
 
 class LoginCest
 {
-    public function _before(FunctionalTester $I)
+    /**
+     * Load fixtures before db transaction begin
+     * Called in _before()
+     * @see \Codeception\Module\Yii2::_before()
+     * @see \Codeception\Module\Yii2::loadFixtures()
+     * @return array
+     */
+    public function _fixtures()
     {
+        return [
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php',
+            ],
+        ];
     }
 
-    // tests
-    public function tryToTest(FunctionalTester $I)
+    public function _before(FunctionalTester $I)
     {
+        $I->amOnRoute('site/login');
+    }
+
+    protected function formParams($login, $password)
+    {
+        return [
+            'LoginForm[username]' => $login,
+            'LoginForm[password]' => $password,
+        ];
+    }
+
+
+    public function checkValidLogin(FunctionalTester $I)
+    {
+        $I->submitForm('#login-form', $this->formParams('rodrigo', 'password_0'));
+        $I->see('Logout (rodrigo)', 'form button[type=submit]');
+        $I->dontSeeLink('Login');
+        $I->dontSeeLink('Registar');
     }
 }
